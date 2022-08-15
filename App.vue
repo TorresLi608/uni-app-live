244ba4583ad3f808c0cab98c29edf9868b66b45d
yuanhu.li <yuanhu.li@hand-china.com>
2022-08-15 10:45:46 +0800

    项目初始化

---
+++ App.vue
@@ -0,0 +1,28 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			// 兼容nvue 引入字体图标
+			const domModule = weex.requireModule('dom')
+			domModule.addRule('fontFace', {
+				'fontFamily': "iconfont",
+				'src': "url('~@/static/icon/iconfnt.ttf')"
+			});
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import url("/common/free.css");
+	@import url("/common/common.css");
+	/* #ifndef APP-PLUS-NVUE */
+	@import url("static/icon/iconfont.css");
+	/* #endif */
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view class="">
+		<text class="iconfont icon-gengduo" style="font-size: 100rpx;"></text>
+	</view>
+</template>
+<script lang="scss">
+	export default {
+		data() {
+			return {
+				title: "title"
+			}
+		}
+	}
+</script>
+<style>
+
+</style>
